7cce719dc8607e7feeba707b90ad935939778794
oangsa <sukruangkul.aongsa@gmail.com>
2022-10-09 21:22:26 +0700

    test mom laptop

--- assign4.py
+++ assign4.py
@@ -1,6 +1,6 @@
 import math
 
-x1, y1, x2, y2 = map(int, input().split())
+x1, y1, x2, y2 = map(int, input("Enter position[x1, y1, x2, y2]: ").split())
 
 eu_dist = float(math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
 mat_dist = float(abs(x1 - x2) + abs(y1 - y2))
